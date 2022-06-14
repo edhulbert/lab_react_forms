de1e3a2ab1dcd3967bac1b0e474bde71fad2b44e
Ed Hulbert <edward.hulbert@hotmail.co.uk>
2022-06-14 14:27:48 +0100

    Added initial files

---
+++ src/containers/RecipeContainer.js
@@ -0,0 +1,36 @@
+import {useState} from "react";
+
+const RecipeContainer = () => {
+    const [recipes, setRecipes] = useState([
+        {
+            cakeName: "Lemon Drizzle",
+            ingredients: ["eggs", "butter", "lemon  zest", "sugar", "self-raising flour"],
+            rating: 5
+        },
+        {
+            cakeName: "Tea Loaf",
+            ingredients: ["eggs", "oil", "dried fruit", "sugar", "self-raising flour", "strong tea"],
+            rating: 3
+        },
+        {
+            cakeName: "Brownie",
+            ingredients: ["chocolate", "eggs", "flour", "butter", "walnuts"],
+            rating: 4
+        },
+        {
+            cakeName: "Carrot Cake",
+            ingredients: ["carrots", "walnuts", "oil", "cream cheese", "flour", "sugar"],
+            rating: 5
+        }
+      ]);
+
+
+    return(
+        <>
+        <p>Hello from inside the Recipe Container!</p>
+        </>
+
+    )
+}
+
+export default RecipeContainer
